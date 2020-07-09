d56f2c81f831695d40dac10e660cc0fd9c6f34b9
Darrell Raymond Ulm <dulm1@kent.edu>
2020-07-09 13:49:40 -0400

    Add the namespace back in and clean up some of the extra spaces added on the blank lines

--- src/IIISierra/APIClient/Sierra.php
+++ src/IIISierra/APIClient/Sierra.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace IIISierra\APIClient;
+
 /**
  * @file Sierra.php
  *
@@ -48,7 +51,7 @@ class Sierra {
     public $config = array(
         'tokenFile' => '/tmp/SierraToken'
     );
-   
+
 /**
  * Constructor
  *
@@ -76,7 +79,7 @@ class Sierra {
 
         $response = $this->_request($this->config['endpoint'] . $resource, $params, $headers, $type);
         if ($response['status'] != 200) return null;
-        
+
         return json_decode($response['body'], true);
     }
 
@@ -90,7 +93,7 @@ class Sierra {
         if (file_exists($this->config['tokenFile'])) {
             $this->token = json_decode(file_get_contents($this->config['tokenFile']), true);
         }
-        
+
         if (!$this->token || (time() >= $this->token['expires_at'])) {
             return $this->_accessToken();
         }
@@ -104,19 +107,19 @@ class Sierra {
  */
     private function _accessToken() {
         $auth = base64_encode($this->config['key'] . ':' . $this->config['secret']);
-    
+
         $response = $this->_request($this->config['endpoint'] . 'token', array('grant_type', 'client_credentials'), array('Authorization: Basic ' . $auth), 'post');
         $token = json_decode($response['body'], true);
         if (!$token) return false;
 
         if (!isset($token['error'])) {
             $token['expires_at'] = time() + $token['expires_in'];
-        
+
             $this->token = $token;
             file_put_contents($this->config['tokenFile'], json_encode($token));
             return true;
         }
-        
+
         return false;
     }
 
@@ -138,7 +141,7 @@ class Sierra {
         $type = strtolower($type);
 
         $s = curl_init();
-        
+
         if ($type == 'post') {
             $header[] = 'Content-Type: text/plain';
             curl_setopt($s, CURLOPT_POST, true);
@@ -154,14 +157,14 @@ class Sierra {
         else {
             $url .= ($params ? '?' . http_build_query($params) : '');
         }
-        
+
         curl_setopt($s, CURLOPT_URL, $url);
         curl_setopt($s, CURLOPT_TIMEOUT, 60);
         curl_setopt($s, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($s, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($s, CURLOPT_USERAGENT, 'Sierra PHP Test/0.1');
         curl_setopt($s, CURLOPT_HEADER, true);
-    
+
         if ($header) {
             curl_setopt($s, CURLOPT_HTTPHEADER, $header);
         }
@@ -171,14 +174,14 @@ class Sierra {
         $headerSize = curl_getinfo($s, CURLINFO_HEADER_SIZE);
         $header = $this->_parseResponseHeaders(substr($result, 0, $headerSize));
         $body = substr($result, $headerSize);
-    
+
         $response = array(
             'status' => $status,
             'header' => $header,
             'body' => $body
         );	
         curl_close($s);
-    
+
         return $response;
     }
 
@@ -207,7 +210,7 @@ class Sierra {
                 }
             }
         }
-    
+
         return $headers;
     }
 }
